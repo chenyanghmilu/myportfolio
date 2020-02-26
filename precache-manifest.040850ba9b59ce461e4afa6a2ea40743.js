3cfcced2536d43cd2bf235ea3890d528ea7e8866
Yang Chen <chenyanghmilu@gmail.com>
2020-02-26 02:26:51 -0500

    Updates

---
+++ precache-manifest.040850ba9b59ce461e4afa6a2ea40743.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4316b0b5028110c76cc30dc34a351767",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "953b8fe72c40b1afcb10",
+    "url": "/myportfolio/static/css/main.d09bce08.chunk.css"
+  },
+  {
+    "revision": "11e3cd8149990f113973",
+    "url": "/myportfolio/static/js/2.1ad2da87.chunk.js"
+  },
+  {
+    "revision": "9a81cbccbe3d87d0aa2be5df1a345376",
+    "url": "/myportfolio/static/js/2.1ad2da87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "953b8fe72c40b1afcb10",
+    "url": "/myportfolio/static/js/main.4dcff993.chunk.js"
+  },
+  {
+    "revision": "c8d0869b998717c93466",
+    "url": "/myportfolio/static/js/runtime-main.0f4b6eff.js"
+  },
+  {
+    "revision": "7ddd858f345d2382c622b8cd537f71c4",
+    "url": "/myportfolio/static/media/citybg.7ddd858f.jpg"
+  }
+]);
