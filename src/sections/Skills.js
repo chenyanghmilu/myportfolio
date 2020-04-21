3b936b087c5106cce84c8708c1b0d40d13d7eb98
Yang Chen <chenyanghmilu@gmail.com>
2020-04-21 11:20:09 -0400

    update README

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -7,54 +7,54 @@ function Skills(props) {
       <div>
         <div className="skill-level">
           <div className="skill">
-            <img className="skill__icon" src="images/language-icons-svg/javascript.svg" alt=""/>
+            <img className="skill__icon" src="images/language-icons-svg/javascript.svg" alt="" />
             <span className="skill__label">JavaScript</span>
           </div>
           <div className="skill">
-            <img className="skill__icon" src="images/language-icons-svg/html5.svg" alt=""/>
+            <img className="skill__icon" src="images/language-icons-svg/html5.svg" alt="" />
             <span className="skill__label">HTML 5</span>
           </div>
           <div className="skill">
-            <img className="skill__icon" src="images/language-icons-svg/css3.svg" alt=""/>
+            <img className="skill__icon" src="images/language-icons-svg/css3.svg" alt="" />
             <span className="skill__label">CSS 3</span>
           </div>
           <div className="skill">
-            <img className="skill__icon" src="images/language-icons-svg/nodejs-seeklogo.com.svg" alt=""/>
+            <img className="skill__icon" src="images/language-icons-svg/nodejs-seeklogo.com.svg" alt="" />
             <span className="skill__label">Node.js</span>
           </div>
           <div className="skill">
-            <img className="skill__icon skill__icon--medium" src="images/language-icons-svg/python.svg" alt=""/>
+            <img className="skill__icon skill__icon--medium" src="images/language-icons-svg/python.svg" alt="" />
             <span className="skill__label">Python</span>
           </div>
           <div className="skill">
-            <img className="skill__icon skill__icon--medium" src="images/language-icons-svg/C_Sharp_logo.svg" alt=""/>
+            <img className="skill__icon skill__icon--medium" src="images/language-icons-svg/C_Sharp_logo.svg" alt="" />
             <span className="skill__label">C#</span>
           </div>
         </div>
         <div className="skill-level">
           <div className="skill">
-            <img className="skill__icon" src="images/language-icons-svg/mongodb-icon.svg" alt=""/>
+            <img className="skill__icon" src="images/language-icons-svg/mongodb-icon.svg" alt="" />
             <span className="skill__label">MongoDB</span>
           </div>
           <div className="skill">
-            <img className="skill__icon" src="images/language-icons-svg/PostgreSQL_logo.3colors.svg" alt=""/>
+            <img className="skill__icon" src="images/language-icons-svg/PostgreSQL_logo.3colors.svg" alt="" />
             <span className="skill__label">PostgreSQL</span>
           </div>
           <div className="skill">
-            <img className="skill__icon" src="images/language-icons-svg/django-logo-positive.svg" alt=""/>
+            <img className="skill__icon" src="images/language-icons-svg/django-logo-positive.svg" alt="" />
             <span className="skill__label">Django</span>
           </div>
           <div className="skill">
-            <img className="skill__icon" src="images/language-icons-svg/react.svg" alt=""/>
+            <img className="skill__icon" src="images/language-icons-svg/react.svg" alt="" />
             <span className="skill__label">React</span>
           </div>
           <div className="skill">
-            <img className="skill__icon" src="images/language-icons-svg/jquery.svg" alt=""/>
+            <img className="skill__icon" src="images/language-icons-svg/jquery.svg" alt="" />
             <span className="skill__label">jQuery</span>
           </div>
           <div className="skill">
-            <img className="skill__icon skill__icon--medium" src="images/language-icons-svg/bootstrap.svg" alt=""/>
-            <span className="skill__label">Bootstrap</span>
+            <img className="skill__icon skill__icon--medium" src="images/language-icons-svg/bootstrap.svg" alt="" />
+            <span className="skill__label">Bootstrap 4</span>
           </div>
         </div>
       </div>
